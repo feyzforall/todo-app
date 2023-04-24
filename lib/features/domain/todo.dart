068a52d8f56215b0cb2dc9dbabe25f2ff751cecc
Feyzullah Kodat <feyzkodat@gmail.com>
2023-04-24 16:27:49 +0300

    created hive object

--- lib/features/domain/todo.dart
+++ lib/features/domain/todo.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 import '../../product/colors.dart';
+import '../../product/hive_constants.dart';
+
+part 'todo.g.dart';
 
 typedef TodoId = String;
 
-class Todo {
+@HiveType(typeId: HiveConstants.todoTypeId)
+class Todo extends HiveObject {
+  @HiveField(0)
   TodoId id;
+  @HiveField(1)
   String title;
+  @HiveField(2)
   String? description;
+  @HiveField(3)
   bool isCompleted;
+  @HiveField(4)
   Color? color;
 
   Todo({
---
+++ lib/product/hive_constants.dart
@@ -0,0 +1,11 @@
+class HiveConstants {
+  static final HiveConstants _appColors = HiveConstants._internal();
+
+  factory HiveConstants() {
+    return _appColors;
+  }
+
+  HiveConstants._internal();
+
+  static const int todoTypeId = 0;
+}
